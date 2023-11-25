04387fb7b51d2fb1aa7b534155661eb9879a90d2
xupingmao <xupingmao@gmail.com>
2023-11-25 11:40:55 +0800

    Add bdwgc

--- src/memory/gc_main.c
+++ src/memory/gc_main.c
@@ -1,7 +1,16 @@
+/*
+ * @Author: xupingmao
+ * @email: 578749341@qq.com
+ * @Date: 2023-11-25 11:23:32
+ * @LastEditors: xupingmao
+ * @LastEditTime: 2023-11-25 11:39:38
+ * @FilePath: \learn-c\src\memory\gc_main.c
+ * @Description: 描述
+ */
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include "../../third_party/bdwgc/include/gc.h"
+#include "../../deps/bdwgc/include/gc.h"
 
 #define malloc GC_malloc
 
@@ -11,14 +20,22 @@ typedef struct object_t {
 } Object;
 
 Object* NewObject(const char* name) {
-    Object* obj = malloc(sizeof(Object));
+    Object* obj = GC_malloc(sizeof(Object));
     obj->size = strlen(name);
-    obj->name = malloc(obj->size+1);
+    obj->name = GC_malloc(obj->size+1);
     strcpy(obj->name, name);
     return obj;
 }
 
+char* ObjectToStr(char* result, Object* obj) {
+    strcat(result, "name=");
+    strcat(result, obj->name);
+    return result;
+}
+
 int main(int argc, char* argv[]) {
     Object *obj = NewObject("hello");
+    char objStr[1024];
+    fprintf("obj=%s", ObjectToStr(objStr, obj));
     return 0;
 }
